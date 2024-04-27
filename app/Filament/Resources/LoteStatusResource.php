d22878452a02421f1cbb8132d5935f18ea5e8263
yosecc <yosec.cervino@gmail.com>
2024-04-27 16:15:47 -0300

    save

--- app/Filament/Resources/LoteStatusResource.php
+++ app/Filament/Resources/LoteStatusResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\LoteStatusResource\Pages;
 use App\Filament\Resources\LoteStatusResource\RelationManagers;
-use App\Models\LoteStatus;
+use App\Models\loteStatus;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
--- app/Filament/Resources/LoteTypeResource.php
+++ app/Filament/Resources/LoteTypeResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\LoteTypeResource\Pages;
 use App\Filament\Resources\LoteTypeResource\RelationManagers;
-use App\Models\LoteType;
+use App\Models\loteType;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
