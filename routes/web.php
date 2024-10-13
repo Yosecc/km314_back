a62e525d5e373f6ef07537112edbee557dd233b3
yosecc <yosec.cervino@gmail.com>
2024-10-13 14:41:08 -0300

    ruta webhook

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::group(['prefix' => 'auth/facebook', 'middleware' => 'auth'], function () 
     Route::get('/', [\App\Http\Controllers\SocialController::class, 'redirectToProvider'])->name('auth.facebook');
     Route::get('/callback', [\App\Http\Controllers\SocialController::class, 'handleProviderCallback']);
 });
-Route::get('/facebook_webhook', [\App\Http\Controllers\SocialController::class, 'facebook_webhook']);
+Route::get('webhook/facebook_webhook', [\App\Http\Controllers\SocialController::class, 'facebook_webhook']);
