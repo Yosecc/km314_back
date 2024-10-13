d9c920cb29e7610485891e3e3740df45932ec83a
yosecc <yosec.cervino@gmail.com>
2024-10-13 20:33:16 -0300

    centerbutton

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -70,7 +70,7 @@ zcio superior para el campo de entrada */
     <div class="chat-container">
         {{-- wire:poll.5s="updateMessages" --}}
         <div id="messages" class="messages-box" >
-            <button type="button" wire:click="pageNext" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action">Mas mensajes</button>
+            <button type="button" wire:click="pageNext" style="align-self:center;display:flex;--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50 fi-ac-action fi-ac-btn-action">Mas mensajes</button>
            
             @foreach (array_reverse($messages->toArray()) as $message)
             <div class="message @if($message['from']['id'] == $userId) sent @else received @endif" wire:key="message-{{ $message['id'] }}">
