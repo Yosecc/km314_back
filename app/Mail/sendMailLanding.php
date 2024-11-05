39b5034b54becdc9c93ad8a8101d2ede42ff063c
yosecc <yosec.cervino@gmail.com>
2024-11-05 00:16:29 -0300

    espacio

--- app/Mail/sendMailLanding.php
+++ app/Mail/sendMailLanding.php
@@ -32,7 +32,7 @@ class sendMailLanding extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Interesados'.$this->landing->title,
+            subject: 'Interesados '.$this->landing->title,
         );
     }
 
