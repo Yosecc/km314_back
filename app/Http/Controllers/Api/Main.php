654cead54bf87e02c0f04be9a8e79990696a1b12
yosecc <yosec.cervino@gmail.com>
2024-12-06 12:50:32 -0300

    slider

--- app/Http/Controllers/Api/Main.php
+++ app/Http/Controllers/Api/Main.php
@@ -23,20 +23,10 @@ class Main extends Controller
 {
     public function sliders()
     {
-
-        return [
-            'https://kilometro314.com/images/casas/30.jpg',
-            'https://kilometro314.com/images/architect/home.jpg',
-            'https://kilometro314.com/images/casas/5.jpg',
-            'https://kilometro314.com/images/casas/6.jpg',
-            'https://kilometro314.com/images/casas/7.jpg',
-
-        ];
         return Slider::where('status',1)->get()->map(function($slide){
             $slide['img'] =  config('app.url').Storage::url($slide['img']);
             return $slide;
         })->pluck('img');
-
     }
 
     public function trabajos(Request $request)
