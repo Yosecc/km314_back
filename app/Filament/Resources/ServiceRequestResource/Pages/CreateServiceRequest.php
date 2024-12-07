07db42b9b52933ef2e7256d683f325ce67f0ba18
yosecc <yosec.cervino@gmail.com>
2024-12-07 18:38:02 -0300

    pruieba notificacacion solicitud

--- app/Filament/Resources/ServiceRequestResource/Pages/CreateServiceRequest.php
+++ app/Filament/Resources/ServiceRequestResource/Pages/CreateServiceRequest.php
@@ -5,8 +5,18 @@ namespace App\Filament\Resources\ServiceRequestResource\Pages;
 use App\Filament\Resources\ServiceRequestResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
-
+use App\Models\User;
+use Filament\Notifications\Notification;
 class CreateServiceRequest extends CreateRecord
 {
     protected static string $resource = ServiceRequestResource::class;
+
+    protected function afterCreate(): void
+    {
+        $recipient = User::whereHas("roles", function($q){ $q->where("name", "super_admin"); })->get();
+
+        Notification::make()
+            ->title('Nueva solicitud')
+            ->sendToDatabase($recipient);
+    }
 }
