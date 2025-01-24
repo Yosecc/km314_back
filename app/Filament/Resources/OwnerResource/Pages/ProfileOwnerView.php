bc8872ce6e6cebdadc75eef73485e22cb251df69
yosecc <yosec.cervino@gmail.com>
2025-01-24 00:15:39 -0300

    permiso

--- app/Filament/Resources/OwnerResource/Pages/ProfileOwnerView.php
+++ app/Filament/Resources/OwnerResource/Pages/ProfileOwnerView.php
@@ -6,10 +6,12 @@ use App\Filament\Resources\OwnerResource;
 use Filament\Actions;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
-
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class ProfileOwnerView extends ViewRecord
 {
+
+    use HasPageShield;
     protected static string $resource = OwnerResource::class;
     protected static ?string $navigationIcon = 'heroicon-o-eye'; // Icono para el menú de navegación
     protected static ?string $navigationLabel = 'Ver perfil'; // Etiqueta para el menú de navegación
