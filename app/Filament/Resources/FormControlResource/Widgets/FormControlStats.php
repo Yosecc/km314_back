f65a7a0d8c74bacb5de0dff7ce45d2ac7584f2f8
yosecc <yosec.cervino@gmail.com>
2025-01-24 00:57:54 -0300

    widget

--- app/Filament/Resources/FormControlResource/Widgets/FormControlStats.php
+++ app/Filament/Resources/FormControlResource/Widgets/FormControlStats.php
@@ -5,14 +5,18 @@ namespace App\Filament\Resources\FormControlResource\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Illuminate\Support\Facades\Auth;
 
 class FormControlStats extends BaseWidget
 {
     use HasWidgetShield;
     protected function getStats(): array
     {
+        $num = FormControl::where('owner_id', Auth::user()->owner_id)->orderBy('created_at', 'desc')->count();
         return [
-            Stat::make('Unique views', '192.1k'),
+            Stat::make('Mis Formularios',$num)
+                ->icon('heroicon-o-document-text')
+                ->link(route('filament.admin.resources.form-control.create')),
         ];
     }
 }
