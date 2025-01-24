f6485d3e40b6b63a9c208d806bbe3fd42f51e2d7
yosecc <yosec.cervino@gmail.com>
2025-01-24 01:16:54 -0300

    otro widget

--- app/Filament/Resources/FormControlResource/Widgets/FormControlStats.php
+++ app/Filament/Resources/FormControlResource/Widgets/FormControlStats.php
@@ -20,6 +20,11 @@ class FormControlStats extends BaseWidget
                 ->description('Crear formulario')
                 ->descriptionIcon('heroicon-o-link')
                 ->url('form-controls/create'),
+            Stat::make('Mi Perfil', 'Ver')
+                ->icon('heroicon-o-user')
+                ->description('Ver perfil')
+                ->descriptionIcon('heroicon-o-link')
+                ->url(route('filament.admin.resources.owners.view-profile-owner', ['record' => auth()->user()->owner_id])),
         ];
     }
 }
