56a421d637d41fd5834dfd6aae586b48b0f8ed6f
yosecc <yosec.cervino@gmail.com>
2025-05-25 00:31:19 -0300

    sss

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -82,7 +82,7 @@ class PaymentResource extends Resource
         //     Group::make('owner_id')
         //         ->getTitleFromRecordUsing(fn (Owner $record): string => ucfirst($record->nombres())),
         // ])
-            ->defaultGroup('owner_id.first_name')
+            ->defaultGroup('owner.first_name')
             ->columns([
                 TextColumn::make('id')->sortable(),
                 TextColumn::make('owner.first_name')->label('Propietario'),
