1335e658bccdea571fc9f590beda58daf27dd94a
yosecc <yosec.cervino@gmail.com>
2025-05-29 14:56:13 -0300

    sss

--- app/Filament/Resources/AccountStatusResource/Widgets/MovementsTableWidget.php
+++ app/Filament/Resources/AccountStatusResource/Widgets/MovementsTableWidget.php
@@ -5,30 +5,11 @@ namespace App\Filament\Resources\AccountStatusResource\Widgets;
 use Filament\Widgets\Widget;
 use Illuminate\Contracts\View\View;
 use Filament\Widgets\TableWidget as BaseWidget;
-use Filament\Tables;
-use Filament\Tables\Table;
-use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
 class MovementsTableWidget extends BaseWidget
 {
     public ?object $record = null;
-    use HasWidgetShield;
-    // protected static string $view = 'filament.resources.account-status-resource.widgets.movements-table-widget';
 
-    public function table(Table $table): Table
-    {
-        $movimientos = collect($this->getMovements());
-        return $table
-            ->paginated([5, 10, 15, 'all'])
-            ->defaultPaginationPageOption(10)
-            ->records($movimientos)
-            ->columns([
-                Tables\Columns\TextColumn::make('fecha')->label('Fecha')->formatStateUsing(fn($state) => \Carbon\Carbon::parse($state)->format('d/m/Y')),
-                Tables\Columns\TextColumn::make('tipo')->label('Tipo')->badge(),
-                Tables\Columns\TextColumn::make('descripcion')->label('Descripción'),
-                Tables\Columns\TextColumn::make('monto')->label('Monto')->formatStateUsing(fn($state, $record) => number_format($state, 2, ',', '.'))
-                    ->color(fn($record) => $record['tipo'] === 'Pago' ? 'success' : ($record['monto'] < 0 ? 'danger' : 'primary')),
-            ]);
-    }
+    protected static string $view = 'filament.resources.account-status-resource.widgets.movements-table-widget';
 
     public function getMovements(): array
     {
@@ -61,10 +42,10 @@ class MovementsTableWidget extends BaseWidget
         return $all->toArray();
     }
 
-    // public function render(): View
-    // {
-    //     return view(static::$view, [
-    //         'movimientos' => $this->getMovements(),
-    //     ]);
-    // }
+    public function render(): View
+    {
+        return view(static::$view, [
+            'movimientos' => $this->getMovements(),
+        ]);
+    }
 }
