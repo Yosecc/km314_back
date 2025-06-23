91c3882ca83138259e130b57205fd9fee0f28764
yosecc <yosec.cervino@gmail.com>
2025-06-23 19:11:12 -0300

    s

--- database/migrations/2025_06_23_185702_create_form_incident_question_category_question_table.php
+++ database/migrations/2025_06_23_185702_create_form_incident_question_category_question_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('form_incident_question_category_question', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('form_incident_question_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('form_incident_category_question_id')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('form_incident_question_id');
+            $table->unsignedBigInteger('form_incident_category_question_id');
             $table->timestamps();
+
+            $table->foreign('form_incident_question_id', 'fiqcc_question_id_fk')
+                ->references('id')->on('form_incident_questions')->onDelete('cascade');
+            $table->foreign('form_incident_category_question_id', 'fiqcc_category_id_fk')
+                ->references('id')->on('form_incident_category_questions')->onDelete('cascade');
         });
     }
 
