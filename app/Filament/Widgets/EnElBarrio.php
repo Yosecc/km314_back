2bff40101b682dcaa203af34608c1fcbd51dd11f
yosecc <yosec.cervino@gmail.com>
2025-07-17 19:40:25 -0300

    sss

--- app/Filament/Widgets/EnElBarrio.php
+++ app/Filament/Widgets/EnElBarrio.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Collection;
 
 class EnElBarrio extends BaseWidget
 {
-    public function getTableRecords(): \Illuminate\Database\Eloquent\Collection
+    public function query()
     {
         $peopleInside = ActivitiesPeople::select('model_id', 'model')
             ->join('activities', 'activities_people.activities_id', '=', 'activities.id')
