ed6bea81feb4044987e7be2958f55684204bbf75
yosecc <yosec.cervino@gmail.com>
2025-09-23 18:19:34 -0300

    s

--- app/Filament/Resources/EmployeeResource/Pages/EditEmployee.php
+++ app/Filament/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditEmployee extends EditRecord
 {
