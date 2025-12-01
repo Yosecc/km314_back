2b98934b3f6f3a15ac96d40d94c92ba3166f48f0
yosecc <yosec.cervino@gmail.com>
2025-12-01 13:21:09 -0300

    s

--- app/Filament/Resources/FormControlResource/Pages/CreateFormControl.php
+++ app/Filament/Resources/FormControlResource/Pages/CreateFormControl.php
@@ -14,31 +14,35 @@ class CreateFormControl extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        dd($data);
-        // if (isset($data['peoples']) && is_array($data['peoples'])) {
-        //     $data['peoples'] = array_map(function ($person) {
-        //         $person['is_responsable'] = isset($person['is_responsable']) ? (bool)$person['is_responsable'] : false;
-        //         $person['is_acompanante'] = isset($person['is_acompanante']) ? (bool)$person['is_acompanante'] : false;
-        //         $person['is_menor'] = isset($person['is_menor']) ? (bool)$person['is_menor'] : false;
-        //         return $person;
-        //     }, $data['peoples']);
-        // }
-        // dd($data);
+        // Las relaciones (peoples, autos, mascotas, files) se guardan automáticamente
+        // después del create cuando usas ->relationship() en los Repeaters
+        
+        // Aquí solo manejas los datos del modelo principal
         return $data;
     }
 
     protected function afterCreate(): void
     {
         try {
-            $recipient = User::whereHas("roles", function($q){ $q->whereIn("name", ["super_admin"]); })->get();
+            // Aquí ya tienes acceso al registro creado con todas sus relaciones
+            $formControl = $this->record;
+            
+            // Puedes acceder a las relaciones guardadas:
+            // $formControl->peoples
+            // $formControl->autos
+            // $formControl->mascotas
+            // $formControl->files
+            
+            $recipient = User::whereHas("roles", function($q){ 
+                $q->whereIn("name", ["super_admin"]); 
+            })->get();
 
             Notification::make()
                 ->title('Se ha creado un nuevo formulario de control')
                 ->sendToDatabase($recipient);
 
         } catch (\Throwable $th) {
-
-        //throw $th;
+            // Manejo de errores
         }
     }
 }
