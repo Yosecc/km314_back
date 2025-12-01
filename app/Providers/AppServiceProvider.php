de692517f26fdbc025c39f111e802d562d694aa3
yosecc <yosec.cervino@gmail.com>
2025-12-01 13:33:09 -0300

    df

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,8 @@ use App\Models\Invoice;
 use App\Observers\InvoiceObserver;
 use App\Models\InvoiceItem;
 use App\Observers\InvoiceItemObserver;
+use App\Models\FormControlPeople;
+use App\Observers\FormControlPeopleObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
         Payment::observe(PaymentObserver::class);
         Invoice::observe(InvoiceObserver::class);
         InvoiceItem::observe(InvoiceItemObserver::class);
+        FormControlPeople::observe(FormControlPeopleObserver::class);
         // $this->loadTranslationsFrom(__DIR__.'/../lang', 'general');
     }
 }
