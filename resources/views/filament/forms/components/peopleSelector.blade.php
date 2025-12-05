f6e80a393e16f3ea4dae9ff52c019590c62d1fc8
yosecc <yosec.cervino@gmail.com>
2025-12-05 10:36:52 -0300

    s

--- resources/views/filament/forms/components/peopleSelector.blade.php
+++ resources/views/filament/forms/components/peopleSelector.blade.php
@@ -76,11 +76,8 @@
                         @if(isset($persona['vencimientos']) && count($persona['vencimientos']) > 0)
                             <div class="mt-3 space-y-2">
                                 @foreach($persona['vencimientos'] as $vencimiento)
-                                    <div class="flex items-start gap-2 p-2 rounded {{ $vencimiento['color_bg'] }} {{ $vencimiento['color_text'] }}">
-                                        <svg class="w-4 h-4 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                        </svg>
-                                        <span class="text-xs font-medium">{{ $vencimiento['texto'] }}</span>
+                                    <div class="text-xs" style="background: #E74C3C; padding: 10px; border-radius: 7px; color: white;">
+                                        {{ $vencimiento['texto'] }}
                                     </div>
                                 @endforeach
                             </div>
