f77852738c4de0ea38d2a409af43a636b89a3249
yosecc <yosec.cervino@gmail.com>
2025-12-05 11:36:08 -0300

    s

--- resources/views/filament/forms/components/peopleSelector.blade.php
+++ resources/views/filament/forms/components/peopleSelector.blade.php
@@ -89,7 +89,7 @@
         </div>
     @else
         <div class="text-center py-8 text-gray-500 dark:text-gray-400">
-            <svg class="mx-auto h-12 w-12 mb-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg class="mx-auto h-12 w-12 mb-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" style="width=50px">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
             </svg>
             <p class="font-medium">No hay personas disponibles</p>
