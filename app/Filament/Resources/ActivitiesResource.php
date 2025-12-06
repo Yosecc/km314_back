a8488ea5bc6a57872744a4e8a49c88245a75672c
yosecc <yosec.cervino@gmail.com>
2025-12-06 14:17:05 -0300

    s

--- app/Filament/Resources/ActivitiesResource.php
+++ app/Filament/Resources/ActivitiesResource.php
@@ -585,12 +585,12 @@ class ActivitiesResource extends Resource
                     Forms\Components\TextInput::make('quick_code')
                         ->label('Código de Acceso Rápido')
                         ->placeholder('Escanea QR o ingresa código (Ej: E-A1B2C3D4)')
-                        ->columnSpan(['sm'=> 2])
-                        ->columnStart([
-                            'sm' => 2,
-                            'xl' => 3,
-                            '2xl' => 4,
-                        ])
+                        // ->columnSpan(['sm'=> 2])
+                        // ->columnStart([
+                        //     'sm' => 2,
+                        //     'xl' => 3,
+                        //     '2xl' => 4,
+                        // ])
                         ->live(onBlur: true)
                         ->afterStateUpdated(function($state, Set $set, Get $get) {
                             if (!$state) return;
