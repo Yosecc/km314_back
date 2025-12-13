4972e7e4bfb6ba9a841999330d1e01d295ff20dc
yosecc <yosec.cervino@gmail.com>
2025-12-13 00:46:32 -0300

    oij

--- app/Filament/Resources/OwnerResource/Widgets/OwnerQr.php
+++ app/Filament/Resources/OwnerResource/Widgets/OwnerQr.php
@@ -4,9 +4,18 @@ namespace App\Filament\Resources\OwnerResource\Widgets;
 
 use Filament\Widgets\Widget;
 use App\Models\Owner;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use App\Traits\HasQrCodeAction;
 
-class OwnerQr extends Widget
+class OwnerQr extends Widget implements HasActions, HasForms
 {
+    use InteractsWithActions;
+    use InteractsWithForms;
+    use HasQrCodeAction;
+    
     protected static string $view = 'filament.widgets.owner-qr';
     
     public ?Owner $record = null;
@@ -16,5 +25,10 @@ class OwnerQr extends Widget
     public function mount(?Owner $record = null): void
     {
         $this->record = auth()->user()->hasRole('owner') ? auth()->user()->owner : $record;
+    }
+    
+    public function showQrAction()
+    {
+        return $this->getQrCodeAction();
     }
 }
--- resources/views/filament/widgets/owner-qr.blade.php
+++ resources/views/filament/widgets/owner-qr.blade.php
@@ -2,40 +2,8 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="flex flex-col items-center justify-center p-6">
-            <x-filament::button
-                x-data=""
-                x-on:click="$dispatch('open-modal', { id: 'qr-owner-modal' })"
-                icon="heroicon-o-qr-code"
-                color="info"
-                size="lg"
-            >
-                Ver Código QR
-            </x-filament::button>
+            {{ ($this->showQrAction)(['record' => $record]) }}
         </div>
     </x-filament::section>
-    
-    <x-filament::modal id="qr-owner-modal" width="lg">
-        <x-slot name="heading">
-            Código de Acceso Rápido
-        </x-slot>
-        
-        <x-slot name="description">
-            Propietario: {{ $record->first_name }} {{ $record->last_name }}
-        </x-slot>
-        
-        @include('components.qr-modal', [
-            'record' => $record,
-            'entityType' => 'Propietario'
-        ])
-        
-        <x-slot name="footerActions">
-            <x-filament::button
-                color="gray"
-                x-on:click="close"
-            >
-                Cerrar
-            </x-filament::button>
-        </x-slot>
-    </x-filament::modal>
 </x-filament-widgets::widget>
 @endif
