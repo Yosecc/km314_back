3ac44fb8b63933040388fa652e10e25aa788aef4
yosecc <yosec.cervino@gmail.com>
2025-12-18 11:04:36 -0300

    s

--- app/Http/Livewire/CustomLogin.php
+++ app/Http/Livewire/CustomLogin.php
@@ -1,33 +1,33 @@
-    protected function getFormSchema(): array
+    <?php
+    namespace App\Http\Livewire;
+
+    use Filament\Pages\Auth\Login as BaseLogin;
+    use Illuminate\Validation\ValidationException;
+
+    class CustomLogin extends BaseLogin
     {
-        return array_merge(
-            [
-                ...parent::getFormSchema(),
-                \Filament\Forms\Components\Checkbox::make('terms')
-                    ->label('Acepto los <a href=\"#\" target=\"_blank\" class=\"underline\">términos y condiciones</a>')
-                    ->required()
-                    ->html(),
-            ]
-        );
-    }
+        public $terms = false;
 
-<?php
-namespace App\Http\Livewire;
-
-use Filament\Pages\Auth\Login as BaseLogin;
-use Illuminate\Validation\ValidationException;
-
-class CustomLogin extends BaseLogin
-{
-    public $terms = false;
-
-    public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
-    {
-        if (! $this->terms) {
-            throw ValidationException::withMessages([
-                'terms' => 'Debes aceptar los términos y condiciones para continuar.',
-            ]);
+        protected function getFormSchema(): array
+        {
+            return array_merge(
+                parent::getFormSchema(),
+                [
+                    \Filament\Forms\Components\Checkbox::make('terms')
+                        ->label('Acepto los <a href=\"#\" target=\"_blank\" class=\"underline\">términos y condiciones</a>')
+                        ->required()
+                        ->html(),
+                ]
+            );
         }
-        return parent::authenticate();
+
+        public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
+        {
+            if (! $this->terms) {
+                throw ValidationException::withMessages([
+                    'terms' => 'Debes aceptar los términos y condiciones para continuar.',
+                ]);
+            }
+            return parent::authenticate();
+        }
     }
-}
